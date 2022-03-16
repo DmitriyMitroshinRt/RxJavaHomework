bb4e28b1799d33b5aaad016e0f64b03bcc8b4dec
Alexander Tarabrin <aleksandr.tarabrin@rt.ru>
2022-03-16 11:54:11 +0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.10"
+    application
+}
+
+group = "me.user"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation ("io.reactivex.rxjava3:rxjava:3.1.3")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "RxJavaHomework"
+
