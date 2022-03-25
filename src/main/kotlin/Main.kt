71bf9a36a1d50aa8461ce4e992ff6edbb34e86a8
gleb molotkov <mail.glb@mail.ru>
2022-03-25 23:40:49 +0300

    first my step

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -24,12 +24,6 @@ fun main() {
     //  это всего лишь имитация, реальное переключение между потоками будет рассмотрено на следующем семинаре
 
     println("Start Q1");
-
-    //Single.timer(1L,TimeUnit.MILLISECONDS).blockingSubscribe({println("0")},{println("error")});
-    //Some Emission
-    //Some Emission
-    //val singleSource = Maybe.just("single item");
-    ///  maybe_array <ByteArray?>= Maybe.fromCallable ( {requestDataFromServerAsync();} );
 /*
     val count = -1
     require(count >= 0)    { println("Count must be non-negative, was $count") }
@@ -47,81 +41,49 @@ fun main() {
     }).blockingSubscribe({s : ByteArray? ->  println("Item received: from Maybe"+s.contentToString());
     },
         { obj: Throwable -> obj.printStackTrace() } ) { println("Done from MaybeSource") }
-
-
-
-    /* singleSource.subscribe(
-         { s: String -> println("Item received: from singleSource $s") },
-         { obj: Throwable -> obj.printStackTrace() }
-     ) { println("Done from SingleSource") }
-     */
-
-
-    // Maybe<ByteArray?> maybe_array = Maybe.just("single item");
-    //  Maybe<ByteArray?>.blockingSubscribe({},{});
-    //  Maybe.fromCallable ( requestDataFromServerAsync() )
     println("Finished Q1");
 
-
     println("Start Q2");
-    //requestServerAsync();
-    //assertTrue( is Unit);
-    /* val callable1 = object : Callable<Int> {
-         override fun call(): Int = 1
-     }
-     val c1 = createCallable(callable1)
-     println("callable1 = ${c1.call()}")
-
-     val callable2 = object : Callable<Unit> {
-         override fun call(): Unit { println("Hello"); throw Exception("Hello") }
-     }
-     val c2 = createCallable(callable2)
-     c2.call()
-
-     */
-
     Completable.fromCallable ( object: Callable<Unit> {
         override fun call(): Unit { requestServerAsync()}} ).
     subscribe({println("Successful");},
         { obj: Throwable -> obj.printStackTrace() } );
-    //.fromCallable(requestServerAsync())
-    //.timer(1L,TimeUnit.MILLISECONDS).blockingSubscribe({println("2")},{println("error2")});
-
-
     println("Finished Q2");
 
     println("Start Q3");
-    //val val1 = requestDataFromDbAsync<Int?>();
+    Single.fromCallable ({
+        var result: Int? =  requestDataFromDbAsync<Int?>()
+         result
+    }
+    ).onErrorComplete{throwable : Throwable -> throwable is NullPointerException}.
+    blockingSubscribe({println("Item received: from Single:$it")}, { obj: Throwable -> obj.printStackTrace() },{println("Item received: from Single:null")});
 
-   /* Single.fromCallable ({
-        var result: Int? = requestDataFromDbAsync<Int?>()    }
-    ).onErrorComplete(println("eee"))
-*/
-    //
-    //onErrorComplete(result: Throwable -> println("--"))
-
-    /* blockingSubscribe({println("Successful");},
-         { obj: Throwable -> obj.printStackTrace() })
-
-     */
-
-
-/*    ({s : Int? ->  println("Item received: from Single:"+s.toString()})
-    { obj: Throwable -> obj.printStackTrace() })
-    */
+    Maybe.fromCallable ({
+        var result: Int? =  requestDataFromDbAsync<Int?>()
+        result
+    }
+    ).
+    blockingSubscribe({println("Item received: from Maybe:$it")}, { obj: Throwable -> obj.printStackTrace() },{println("Item received: from Maybe: null")});
     println("Finish Q3");
 
     println("Start Q4");
-    emitEachSecond();
+    //emitEachSecond();
     println("Finish Q4");
 
-
+    println("Start Q5");
+    xMap { flatMapCompletable(it) }
+    xMap { concatMapCompletable (it) }
+    xMap { switchMapCompletable(it) }
+    println("Finish Q5");
 
 
 }
 
+
+
 // 1) Какой источник лучше всего подойдёт для запроса на сервер, который возвращает результат?
-//  Maybe
+//  Maybe - ну результат он возвращает(испускает более правильно в этом паттерне) ..
+//  ну и null  в отличии от Single допускает...
 // Почему?
 // Согласно лекции номер 2
 // Дописать функцию
@@ -131,8 +93,8 @@ fun requestDataFromServerAsync() : ByteArray? /* -> ???<ByteArray> */ {
     fun getDataFromServerSync(): ByteArray? {
         Thread.sleep(LATENCY);
         val success = Random.nextBoolean()
-        //return if (success) Random.nextBytes(RESPONSE_LENGTH) else null
-        return  Random.nextBytes(RESPONSE_LENGTH)
+        return if (success) Random.nextBytes(RESPONSE_LENGTH) else null
+        //return  Random.nextBytes(RESPONSE_LENGTH)
 
     }
     return getDataFromServerSync()
@@ -143,7 +105,8 @@ fun requestDataFromServerAsync() : ByteArray? /* -> ???<ByteArray> */ {
 // 2) Какой источник лучше всего подойдёт для запроса на сервер, который НЕ возвращает результат?
 //Completable
 // Почему?
-// По определению ..
+// По определению ..- заточен на какое то действие без возврата результата - а ля типа procedure vs function
+// по реализации - можно  завернуть  иксепшен что бы не делать сверху генерации ошибки - ну если это надо.
 // Дописать функцию
 fun requestServerAsync() :Unit  /* -> ??? */ {
 
@@ -157,21 +120,47 @@ fun requestServerAsync() :Unit  /* -> ??? */ {
 }
 
 // 3) Какой источник лучше всего подойдёт для однократного асинхронного возвращения значения из базы данных?
-//Single
+//   Можно конечно использовать Maybe- Но неуверен насчет однократного ? (возвращает null в отличии  Single ) НО
+// Single согласно документации одно "испускание" делает - соответственно через обработчик ошибки
+// на null - обойдем это
+// Сделал и так и этак - см. выше
 // Почему?
-//Single — реактивный Callable, потому что тут появляется возможность вернуть результат операции. Продолжая сравнение с Kotlin, можно сказать, что Single — это fun single(): T { }. Таким образом, чтобы подписаться на него, необходимо реализовать onSuccess(T) и onError.
+fun <T> requestDataFromDbAsync1() /* -> ??? */ {
+    // Функция имитирует синхронный запрос к БД не возвращающий результата
+    fun getDataFromDbSync(): T? {
+        Thread.sleep(LATENCY); return null
+    }
+    //NullPointerException
+    /* return */
+    return Maybe.fromCallable { getDataFromDbSync() }
+        .blockingSubscribe(
+            { println(":"+it) }, { println("database request error") }, { println("database request complete") })
+}
 
 // Дописать функцию
 fun <T> requestDataFromDbAsync() : T? /* -> ??? */ {
 
     // Функция имитирует синхронный запрос к БД не возвращающий результата
     fun getDataFromDbSync(): T? {
+         //val s : Int? = 1
         Thread.sleep(LATENCY); return null
     }
     return getDataFromDbSync()
+
     /* return */
 }
+// Дописать функцию
+fun <T> requestDataFromDbAsync2() : Int? /* -> ??? */ {
 
+    // Функция имитирует синхронный запрос к БД не возвращающий результата
+    fun getDataFromDbSync(): Int? {
+        //val s : Int? = 1
+        Thread.sleep(LATENCY); return null
+    }
+    return getDataFromDbSync()
+
+    /* return */
+}
 // 4) Примените к источнику оператор (несколько операторов), которые приведут к тому, чтобы элемент из источника
 // отправлялся раз в секунду (оригинальный источник делает это в 2 раза чаще).
 // Значения должны идти последовательно (0, 1, 2 ...)
@@ -199,8 +188,16 @@ fun emitEachSecond() {
 // Вызов осуществлять поочерёдно из функции main
 //
 //  xMap { flatMapCompletable(it) }
+// возвращает (emit?) результат чисто внешне в рандомном порядке
+// в терминологии этого паттерна -  генерит один стрим(несколько) и маппиться напрямую в конечный стрим без гарантии сохранения порядка
 //  xMap { concatMapCompletable (it) }
+// возвращает (emit?) результат чисто внешне в отсортированном по возрастании порядке
+// в терминологии этого паттерна -  генерит один стрим(несколько) и маппиться напрямую в конечный стрим сохраняя порядок
 //  xMap { switchMapCompletable(it) }
+// возвращает (emit?) результат чисто внешне обрезает и отдает крайний результат
+// в терминологии этого паттерна(не уверен)  -  генерит один стрим(несколько) элементв видно не пропускаються и маппиться крайний элемент по порядку.
+// Говорят нужно например при вводе символов ....
+//
 //
 fun xMap(mapper: Flowable<Int>.(internalMapper: (Int) -> Completable) -> Completable) {
 
